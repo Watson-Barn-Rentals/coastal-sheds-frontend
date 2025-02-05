eb9ca0fdecdb5709aa0bdd25329fdaf3bc34efdf
Nathaniel Rudolph <nathaniel@watsonrentals.net>
2025-02-05 13:06:26 -0600

    backup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: false },
+    runtimeConfig: {
+        public: {
+            // 
+        }
+    },
+    devServer: {
+        host: '0.0.0.0', // This makes the app available on the LAN
+        port: 3000,      // Port number
+    }
 })
